18117b74a8aa401b08d13464e59fe08ee5d8f778
scassady8 <101137225+scassady8@users.noreply.github.com>
2022-07-12 16:50:30 -0500

    Fixed error message

--- src/Meaning.js
+++ src/Meaning.js
@@ -8,12 +8,12 @@ export default function Meaning(props) {
             {props.meaning.definitions.map(function(definition, index){
                 return (
                     <div key={index}>
-                        <p>
+                        
                            <strong>Definition:</strong> {definition.definition}
                             <br />
                             <em>{definition.example}</em> 
                             <Synonyms synonyms={definition.synonyms} />
-                        </p>
+                    
                     </div>
                 ); 
             })}                          
